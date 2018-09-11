d17aa15bcb98d182921fa9191f4ad5a1e1e61314
JustAnotherArchivist <JustAnotherArchivist@users.noreply.github.com>
2018-09-11 12:44:07 +0200

    Version 0.1.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
 	name = 'snscrape',
-	version = '0.1.1',
+	version = '0.1.2',
 	description = 'A social networking service scraper',
 	author = 'JustAnotherArchivist',
 	url = 'https://github.com/JustAnotherArchivist/snscrape',
