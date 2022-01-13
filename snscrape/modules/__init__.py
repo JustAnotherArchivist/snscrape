ba7f4f854b7e7b918dca3948ba84af596ed21505
Michael Mehrtens <michael.mehrtens@credera.com>
2022-01-13 10:04:54 -0600

    Use direct imports instead of listdir based imports for modules

--- snscrape/modules/__init__.py
+++ snscrape/modules/__init__.py
@@ -1,15 +1,4 @@
-import importlib
-import os
-import snscrape.base
+from .. import base
+from . import facebook, instagram, vkontakte, weibo, mastodon, twitter, telegram
 
-
-def _import_modules():
-	files = os.listdir(__path__[0])
-	for fn in files:
-		if fn.endswith('.py') and fn != '__init__.py':
-			# Import module if not already imported
-			moduleName = f'snscrape.modules.{fn[:-3]}'
-			module = importlib.import_module(moduleName)
-
-
-_import_modules()
+__all__ = ["facebook", "instagram", "vkontakte", "weibo", "mastodon", "twitter", "telegram", "base"]
